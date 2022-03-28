13ce983eaa5a31cdb1ef3cfb27fed70825492180
Rubén Frías <rubenfrias@Rubens-MacBook-Pro.local>
2022-03-27 20:03:43 -0400

    Changing section containers

--- components/StandOutSection.js
+++ components/StandOutSection.js
@@ -9,20 +9,22 @@ const StandOutSection = () => {
         />
       </div>
       <div className="w-1/2 xl:pl-40 xl:pt-36 2xl:pt-48">
-        <h1 className="font-fraunces text-veryDarkDesBlue font-black mb-10 xl:text-4xl 2xl:text-5xl xl:pr-10 2xl:pr-64">
-          Stand out to the right audience
-        </h1>
-        <p className="text-darkGrayBlue font-barlow xl:text-lg 2xl:text-2xl xl:pr-28 2xl:pr-48">
-          Using a collaborative formula of designers, researchers,
-          photographers, videographers, and copywriters, we'll build and extend
-          your brand in digital places.
-        </p>
-        <a
-          className="font-fraunces font-black relative z-10 xl:text-lg 2xl:text-xl block mt-10 after:content-[''] after:bg-softRed after:h-3 after:w-40 after:absolute after:inset-0 after:top-4 after:z-[-1] after:rounded after:translate-x-[-6px] after:opacity-50 hover:after:opacity-100"
-          href="#"
-        >
-          LEARN MORE
-        </a>
+        <div className="mx-auto lg:max-w-sm xl:max-w-full lg:mt-16">
+          <h1 className="font-fraunces text-veryDarkDesBlue font-black mb-10 lg:text-3xl xl:text-4xl 2xl:text-5xl xl:pr-10 2xl:pr-64">
+            Stand out to the right audience
+          </h1>
+          <p className="text-darkGrayBlue font-barlow xl:text-lg 2xl:text-2xl xl:pr-28 2xl:pr-48">
+            Using a collaborative formula of designers, researchers,
+            photographers, videographers, and copywriters, we'll build and
+            extend your brand in digital places.
+          </p>
+          <a
+            className="font-fraunces font-black relative z-10 w-max lg:after:w-32 xl:after-w-36 xl:text-lg 2xl:text-xl block mt-10 after:content-[''] after:bg-softRed after:h-3 xl:after:w-36 2xl:after:w-40 after:absolute after:inset-0 after:top-4 after:z-[-1] after:rounded lg:after:translate-x-[-4px] xl:after:translate-x-[-6px] after:opacity-50 hover:after:opacity-100"
+            href="#"
+          >
+            LEARN MORE
+          </a>
+        </div>
       </div>
     </div>
   );
--- components/TransformSection.js
+++ components/TransformSection.js
@@ -1,21 +1,23 @@
 const TransformSection = () => {
   return (
     <div className="flex justify-center">
-      <div className="w-1/2 xl:pl-40 xl:pt-36 2xl:pt-48">
-        <h1 className="font-fraunces text-veryDarkDesBlue font-black mb-10 xl:text-4xl 2xl:text-5xl xl:pr-40 2xl:pr-64">
-          Transform your brand
-        </h1>
-        <p className="text-darkGrayBlue font-barlow xl:text-lg 2xl:text-2xl xl:pr-28 2xl:pr-48">
-          We are a full-service creative agency specializing in helping brands
-          grow fast. Engage your clients through compelling visuals that do most
-          of the marketing for you.
-        </p>
-        <a
-          className="font-fraunces font-black relative z-10 xl:text-lg 2xl:text-xl block mt-10 after:content-[''] after:bg-yellow after:h-3 after:w-40 after:absolute after:inset-0 after:top-4 after:z-[-1] after:rounded after:translate-x-[-6px] after:opacity-50 hover:after:opacity-100"
-          href="#"
-        >
-          LEARN MORE
-        </a>
+      <div className="w-1/2">
+        <div className="mx-auto lg:max-w-xs xl:max-w-3xl lg:mt-16">
+          <h1 className="font-fraunces text-veryDarkDesBlue font-black mb-10 lg:text-3xl xl:text-4xl 2xl:text-5xl xl:pr-40 2xl:pr-64">
+            Transform your brand
+          </h1>
+          <p className="text-darkGrayBlue font-barlow xl:text-lg 2xl:text-2xl xl:pr-28 2xl:pr-48">
+            We are a full-service creative agency specializing in helping brands
+            grow fast. Engage your clients through compelling visuals that do
+            most of the marketing for you.
+          </p>
+          <a
+            className="font-fraunces font-black relative w-max z-10 xl:text-lg 2xl:text-xl block mt-10 after:content-[''] after:bg-yellow after:h-3 lg:after:w-32 xl:after:w-36 2xl:after:w-40 after:absolute after:inset-0 after:top-4 after:z-[-1] after:rounded lg:after:translate-x-[-4px] xl:after:translate-x-[-6px] after:opacity-50 hover:after:opacity-100"
+            href="#"
+          >
+            LEARN MORE
+          </a>
+        </div>
       </div>
       <div className="w-1/2">
         <img
